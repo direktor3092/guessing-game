61456e15fc337377049e15a5d05ebb88e69a6cea
Alexei Shahov <93624312+direktor3092@users.noreply.github.com>
2022-08-13 13:24:27 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
+
 
     setRange(min, max) {
-
+        this.left = min
+        this.right = max
+        return this.left, this.right
     }
 
     guess() {
-
+    this.mid = this.left+(Math.ceil((this.right - this.left) / 2))
+    return this.mid
     }
 
     lower() {
-
+       this.right = this.mid
+       return this.mid
     }
 
     greater() {
-
+        this.left = this.mid
+        return this.mid
     }
 }
 
